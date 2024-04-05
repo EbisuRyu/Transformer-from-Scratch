3fddb0d482bd660357b1b15f113542132a9972e0
LONG <doituyentoanlequydon@gmail.com>
2024-04-05 20:13:25 +0700

    Change download_pretrained.py and config.py

--- script/download_pretrained.py
+++ script/download_pretrained.py
@@ -1,7 +1,7 @@
 import gdown
 def download_pretrained_model(epoch = 20):
     url = 'https://drive.google.com/uc?id=1cbhg7wphS8OnM08Fg1SFpp8d_Z11W8cV'
-    output = f'./Transformer from Scratch/model/model_ckpt_epoch{epoch}.pth'
+    output = f'../Transformer from Scratch/model/model_ckpt_epoch{epoch}.pth'
     gdown.download(url, output, quiet = False)
     
 download_pretrained_model()
