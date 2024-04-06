2225fcb36c37aaba1a41304f269a93d2b331847d
LONG <doituyentoanlequydon@gmail.com>
2024-04-06 09:01:06 +0700

    pretrained

--- script/download_pretrained.py
+++ script/download_pretrained.py
@@ -1,7 +1,6 @@
 import gdown
 def download_pretrained_model(epoch = 60):
-    url = 'https://drive.google.com/file/d/1U5VVB8n0H93Y0lVwgqnYP3QGZMehu6sH'
+    url = 'https://drive.google.com/uc?id=1U5VVB8n0H93Y0lVwgqnYP3QGZMehu6sH'
     output = f'/kaggle/working/Transformer-from-Scratch/model/model_ckpt_epoch{epoch}.pt'
     gdown.download(url, output, quiet = False)
-    
 download_pretrained_model()
