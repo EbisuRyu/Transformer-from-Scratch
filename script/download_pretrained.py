a5959ee0b959105715820dbfa790f2ab998d65d1
LONG <doituyentoanlequydon@gmail.com>
2024-04-10 13:24:01 +0700

    Fixing save path

--- script/download_pretrained.py
+++ script/download_pretrained.py
@@ -1,6 +1,6 @@
 import gdown
 def download_pretrained_model(epoch = 60):
     url = 'https://drive.google.com/uc?id=1Be1791q3DZj8YRwXHJgRZ14v48B5KRat'
-    output = f'/content/Transformer-from-Scratch/model/model_ckpt_epoch{epoch}.pt'
+    output = f'./Transformer-from-Scratch/model/model_ckpt_epoch{epoch}.pt'
     gdown.download(url, output, quiet = False)
 download_pretrained_model()
