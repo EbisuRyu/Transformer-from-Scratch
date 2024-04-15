c1be4a16f88ecd0bdb6e137339c609c1a2e98c76
LONG <doituyentoanlequydon@gmail.com>
2024-04-15 17:29:49 +0700

    Adding Wandb finish

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     Path(wandb.config.RUNS_FOLDER_PTH).mkdir(parents = True, exist_ok = True)
     Path(wandb.config.SAVE_MODEL_DIR).mkdir(parents = True, exist_ok = True)
     train_model(wandb.config)
+    wandb.finish()
 
 if __name__ == '__main__':
         main()
