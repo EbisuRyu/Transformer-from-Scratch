7bfed554e35632aeb8feb58adfd9f48dce28a074
LONG <doituyentoanlequydon@gmail.com>
2024-04-16 14:30:05 +0700

    Fixing path

--- script/download_pretrained.py
+++ script/download_pretrained.py
@@ -4,6 +4,6 @@ from pathlib import Path
 def download_pretrained_model(epoch = 150):
     url = 'https://drive.google.com/uc?id=1Be1791q3DZj8YRwXHJgRZ14v48B5KRat'
     output = f'./Transformer-from-Scratch/model/model_ckpt_epoch{epoch}.pt'
-    Path(output).mkdir(parents = True, exist_ok = True)
+    Path('./Transformer-from-Scratch/model/').mkdir(parents = True, exist_ok = True)
     gdown.download(url, output, quiet = False)
 download_pretrained_model()
